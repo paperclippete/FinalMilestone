d3195d593f403a3fa5b1da075b4a6530f57daf5c
paperclippete <alexa.gibson@outlook.com>
2019-08-31 20:50:59 +0000

    created user book event and delete booking view

--- events/views.py
+++ events/views.py
@@ -3,8 +3,8 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import CreateEventForm, JoinEvent
 from bootstrap_modal_forms.generic import BSModalCreateView, BSModalReadView
-from django.urls import reverse_lazy
-from .models import Event
+from django.urls import reverse, reverse_lazy
+from .models import Event, Participant
 
 # Create your views here.
 @login_required
@@ -31,25 +31,42 @@ def post_event(request):
 def view_one_event(request, pk):
     """Displays event information for site user"""
     event = get_object_or_404(Event, pk=pk)
+    user = request.user
     join_form = JoinEvent(request.POST or None)
+    user_joined = Participant.objects.filter(event=event).filter(user=user)
+    user_queue = Participant.objects.filter(event=event).count()
+    current_places = event.max_participants - user_queue
     context = {
         'join_form': join_form,
-        'event': event
+        'event': event,
+        'user_joined': user_joined,
+        'current_places': str(current_places)
     }
 
     # Allow user to join an event    
    
     if request.method == "POST":
         if join_form.is_valid():
-            join_form.instance.user = request.user
+            join_form.instance.user = user
             join_form.instance.event = event
             join_form.save()
             messages.success(request, f"You have signed up for { event.title }")
+            print(current_places)
+            return redirect(view_one_event, pk)
         else:
             messages.error(request, f"Error: Try again later!")
       
     return render(request, 'view_one_event.html', context)
     
+
+def delete_participant(request, pk):
+    """Removes user from event"""
+    event = get_object_or_404(Event, pk=pk)
+    user = request.user
+    participant = Participant.objects.filter(event=event).filter(user=user)
+    participant.delete()
+    messages.success(request, f"You are no longer booked for { event.title }")
+    return redirect('view_all_events')
     
     
      
--- search/urls.py
+++ search/urls.py
@@ -7,5 +7,6 @@ from events import views
 urlpatterns = [
     path('view_all_events/', view_all_events, name='view_all_events'),
     path('view_one_event/<int:pk>', views.view_one_event, name='view_one_event'),
+    path('delete_participant/<int:pk>', views.delete_participant, name='delete_participant'),
     
 ]
