84d42d3c7f42859a867b4e3034fc07851005d27d
paperclippete <alexa.gibson@outlook.com>
2019-08-31 21:44:43 +0000

    added search box to index and created search view

--- search/views.py
+++ search/views.py
@@ -2,10 +2,20 @@ from django.shortcuts import render
 from events.models import Event
 
 # Create your views here.
+def search(request):
+    """Returns events searched by the user"""
+    events = Event.objects.filter(title__icontains=request.GET['query'])
+    context = {
+        'events': events
+    }
+    return render(request, 'view_all_events.html', context)
+
 
 def view_all_events(request):
+    """Returns all events in the database"""
     events = Event.objects.all()
     context = {
         'events': events
     }
     return render(request, 'view_all_events.html', context)
+    
