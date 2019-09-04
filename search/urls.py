0491747f6f798c95692e221a5dd7d68df491e097
paperclippete <alexa.gibson@outlook.com>
2019-09-04 11:40:32 +0000

    moved view one event urls to event app

--- search/urls.py
+++ search/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path, include
 from .views import search, view_all_events
-from events import views
+
 
 # Specific accounts related URLs
 
 urlpatterns = [
     path('', search, name='search'),
     path('view_all_events/', view_all_events, name='view_all_events'),
-    path('view_one_event/<int:pk>', views.view_one_event, name='view_one_event'),
-    path('delete_participant/<int:pk>', views.delete_participant, name='delete_participant'),
-    
+   
 ]
