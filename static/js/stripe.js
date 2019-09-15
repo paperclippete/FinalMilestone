8c06d291e149c364d024c930ef6c9c0e7efdee1a
paperclippete <alexa.gibson@outlook.com>
2019-09-15 14:14:41 +0000

    updated stripe.js to display a card element for payment form

--- static/js/stripe.js
+++ static/js/stripe.js
@@ -1,32 +1,76 @@
-$(function() {
-    $("#payment-form").submit(function() {
-        var form = this;
-        var card = {
-            number: $("#id_credit_card_number").val(),
-            expMonth: $("#id_expiry_month").val(),
-            expYear: $("#id_expiry_year").val(),
-            cvc: $("#id_cvv").val()
-        };
-    
-    Stripe.createToken(card, function(status, response) {
-        if (status === 200) {
-            $("#credit-card-errors").hide();
-            $("#id_stripe_id").val(response.id);
+// Create a Stripe client.
+var formToken = document.querySelector("#payment-form");
+var stripe = Stripe(formToken.dataset.token);
 
-            // Prevent the credit card details from being submitted
-            // to our server
-            $("#id_credit_card_number").removeAttr('name');
-            $("#id_cvv").removeAttr('name');
-            $("#id_expiry_month").removeAttr('name');
-            $("#id_expiry_year").removeAttr('name');
 
-            form.submit();
-        } else {
-            $("#stripe-error-message").text(response.error.message);
-            $("#credit-card-errors").show();
-            $("#validate_card_btn").attr("disabled", false);
+// Create an instance of Elements.
+var elements = stripe.elements();
+
+// Custom styling can be passed to options when creating an Element.
+// (Note that this demo uses a wider set of styles than the guide below.)
+var style = {
+    base: {
+        color: '#32325d',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#fa755a',
+        iconColor: '#fa755a'
+    }
+};
+
+// Create an instance of the card Element.
+var card = elements.create('card', {
+    style: style
+});
+
+// Add an instance of the card Element into the `card-element` <div>.
+card.mount('#card-element');
+console.log("1")
+// Handle real-time validation errors from the card Element.
+card.addEventListener('change', function(event) {
+    var displayError = document.getElementById('card-errors');
+    if (event.error) {
+        displayError.textContent = event.error.message;
+    }
+    else {
+        displayError.textContent = '';
+    }
+});
+console.log("2")
+// Handle form submission.
+var form = document.getElementById('payment-form');
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    stripe.createToken(card).then(function(result) {
+        if (result.error) {
+            // Inform the user if there was an error.
+            var errorElement = document.getElementById('card-errors');
+            errorElement.textContent = result.error.message;
+        }
+        else {
+            // Send the token to your server.
+            stripeTokenHandler(result.token);
         }
     });
-    return false;
-    });
 });
+
+// Submit the form with the token ID.
+function stripeTokenHandler(token) {
+    // Insert the token ID into the form so it gets submitted to the server
+    var form = document.getElementById('payment-form');
+    var hiddenInput = document.createElement('input');
+    hiddenInput.setAttribute('type', 'hidden');
+    hiddenInput.setAttribute('name', 'stripeToken');
+    hiddenInput.setAttribute('value', token.id);
+    form.appendChild(hiddenInput);
+
+    // Submit the form
+    form.submit();
+}
