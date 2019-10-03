aa6db0014b4e359101b6e4ba9b578ecee58a01bb
paperclippete <alexa.gibson@outlook.com>
2019-10-03 13:43:44 +0000

    install coverage and tests passing

--- accounts/tests.py
+++ accounts/tests.py
@@ -24,17 +24,25 @@ class TestUserLoginForm(TestCase):
 class TestUserRegistrationForm(TestCase):
     """ Test User Registration Form is valid and provides correct errors """
     def test_register_form_valid(self):
-        form = UserRegistrationForm(
-            {"username": "TestUser", "email": "Test@Email.com",
-                "first_name": "TestFirstName", "last_name": "TestLastName",
-                "password1": "TestPassword", "password2": "TestPassword"})
+        form = UserRegistrationForm({
+            "username": "TestUser", 
+            "email": "Test@Email.com",
+            "first_name": "TestFirstName", 
+            "last_name": "TestLastName",
+            "password1": "TestPassword", 
+            "password2": "TestPassword"
+        })
         self.assertTrue(form.is_valid())
 
     def test_passwords_do_not_match(self):
-        form = UserRegistrationForm(
-            {"username": "TestUser", "email": "Test@Email.com",
-                "first_name": "TestFirstName", "last_name": "TestLastName",
-                "password1": "TestPassword", "password2": "TestPass"})
+        form = UserRegistrationForm({
+            "username": "TestUser", 
+            "email": "Test@Email.com",
+            "first_name": "TestFirstName", 
+            "last_name": "TestLastName",
+            "password1": "TestPassword", 
+            "password2": "TestPass"
+        })
         self.assertFalse(form.is_valid())
         self.assertEqual(
             form.errors["password2"], [u"The two password fields didn't match."])
--- membership/tests.py
+++ membership/tests.py
@@ -1,3 +1,55 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.shortcuts import reverse
+from django.contrib import auth
+from .forms import OrderMembershipForm
+from django.contrib.auth.models import User
+from .models import Membership
 
-# Create your tests here.
+# Testing Membership Forms
+
+class TestOrderMembershipForm(TestCase):
+    """ Test Order Membership Form is valid and provides correct errors """
+    def test_order_form_valid(self):
+        test_user = User.objects.create(username="TestUser", password="TestPassword")
+        membership = Membership.objects.create(user_id='1')
+        membership_level = 'bronze'
+        self.client.force_login(test_user)
+        form = OrderMembershipForm({
+            "first_name": "TestName",
+            "last_name": "TestLast",
+            "town_or_city": "Hamilton",
+            "street_address1": "67 Lockhart Street",
+            "street_address2": "",
+            "postcode": "ML3 6PU"
+        })
+        self.assertTrue(form.is_valid())
+        
+    def test_order_form_not_valid(self):
+        test_user = User.objects.create(username="TestUser", password="TestPassword")
+        membership = Membership.objects.create(user_id='1')
+        membership_level = 'bronze'
+        self.client.force_login(test_user)
+        form = OrderMembershipForm({
+            "first_name": "TestName",
+            "last_name": "TestLast",
+            "town_or_city": "Hamilton",
+            "street_address1": "67 Lockhart Street",
+            "street_address2": "",
+            "postcode": ""
+        })
+        self.assertFalse(form.is_valid())
+        self.assertEqual(
+            form.errors["postcode"], [u"This field is required."])
+
+
+# Test Membership Views
+
+class TestMembershipViews(TestCase):
+    """ Test Upgrade Membership Form is displayed """
+    def test_get_profile_page(self):
+        test_user = User.objects.create(username="TestUser", password="TestPassword")
+        membership = Membership.objects.create(user_id='1')
+        self.client.force_login(test_user)
+        page = self.client.get("/membership/upgrade/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "membership.html")
