e92db1eb53bef5ab4e552184cb6eae92a1557c66
paperclippete <alexa.gibson@outlook.com>
2019-10-10 19:47:24 +0000

    remove import env - travis fails

--- lanarkshire/settings.py
+++ lanarkshire/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-if 'PRODUCTION' not in os.environ:
-    import env
+# if 'PRODUCTION' not in os.environ:
+#     import env
 import dj_database_url
 
 
@@ -56,7 +56,6 @@ INSTALLED_APPS = [
     'easy_maps',
     'membership',
     'storages',
-    'widget_tweaks'
     
 ]
 
