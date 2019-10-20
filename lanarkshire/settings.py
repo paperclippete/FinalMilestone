beb85157ebe81e6ffaa155b151ed946544a970b8
paperclippete <alexa.gibson@outlook.com>
2019-10-20 19:10:09 +0000

    settings ready for production

--- lanarkshire/settings.py
+++ lanarkshire/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 
 if os.getenv('DEVELOPMENT') == True:
-DEBUG = True
+    DEBUG = True
 else:
     DEBUG = False
 
